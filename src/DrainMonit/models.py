2ea63c375494a32326c92ad8caa2e1bb302581b3
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2019-09-28 22:36:22 +0530

    Adding the basic backend models

--- src/DrainMonit/models.py
+++ src/DrainMonit/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Pipe(models.Model):
+    MATERIAL = (
+        ('HDPE', 'high-density polyethylene'),
+        ('PVC', 'polyvinyl chloride'),
+        ('Clay', 'clay'),
+        ('Concrete', 'concrete'),
+        ('Steel', 'steel'),
+    )
+    location        = models.CharField("Location", max_length=60, null=True)
+    diameter        = models.CharField("Diameter", max_length=60, null=True)
+    elevation       = models.FloatField("Elevation", null=True)
+    material        = models.CharField("Material", max_length=60, choices=MATERIAL)
+    color           = models.CharField("Color", max_length=60, null=True)
+
+class Node(models.Model):
+    location        = models.CharField("Location", max_length=60, null=True)
+    pipe            = models.ForeignKey(Pipe, on_delete=models.CASCADE)
+
+class Name(models.Model):
+    location        = models.CharField("Location", max_length=60, null=True)
