216daaf692b09908de37bbfa9ad1f12048e75265
rohanchacko <rohanchacko@github.com>
2019-09-29 02:23:33 +0530

    Modified template

--- src/DrainMonit/urls.py
+++ src/DrainMonit/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('details', views.details, name='details'),
+    path('sensor/<int:pk>', views.sensor, name='sensor'),
     path('predict', views.predict, name='predict'),
 ]
